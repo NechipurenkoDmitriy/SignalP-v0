a1c7d85277ca8f1c747fff261a144276ab43938a
NechipurenkoDmitriy <pikinaplecho@gmail.com>
2021-11-09 19:40:45 +0300

    third exportImport

--- modules/helpers_array.js
+++ modules/helpers_array.js
@@ -1,15 +1,19 @@
-export function getValue(array, key) {
-  let res = []
-  for (let i = 0; i < array.length; i++) {
-    res.push(array[i][key])
-  }
-  return res
+const helpers = {
+  getValue2: function getValue(array, key) {
+    let res = []
+    for (let i = 0; i < array.length; i++) {
+      res.push(array[i][key])
+    }
+    return res
+  },
+  mapArray: function mapArray(array, key) {
+    let result = {}
+    for (let i = 0; i < array.length; i++) {
+      result[array[i][key]] = array[i]
+    }
+    return result
+  },
 }
 
-export function mapArray(array, key) {
-  let result = {}
-  for (let i = 0; i < array.length; i++) {
-    result[array[i][key]] = array[i]
-  }
-  return result
-}
+export const getValue = helpers.getValue2
+export const mapArray = helpers.mapArray
